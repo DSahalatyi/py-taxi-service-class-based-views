0ecbe9449ead6924ef4c2054fc96166a6af589d4
DSahalatyi <dsahalatyi@gmail.com>
2024-08-28 21:46:57 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    def get_absolute_url(self) -> str:
+        return reverse("taxi:driver-detail", args=[str(self.id)])
 
 
 class Car(models.Model):
